97b95105a9ea34ca7145d8741ec5f482266ff096
Lee Jae Hyeong <littleplayer777@gmail.com>
2023-04-15 00:01:10 +0900

    프로젝트 파일 업로드
    플레이어 좌/우 이동 구현
    점프 구현

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float moveSpeed = 5f;
+	public float jumpForce = 5f;
+	public int maxJumps = 1; // 멀티점프 기능 추가
+	private int jumpCount = 0;
+	public bool isJumping = false;
+	private Rigidbody2D rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void Update()
+	{
+		float moveX = Input.GetAxis("Horizontal") * moveSpeed;
+
+		// 방향키로 좌, 우 이동
+		if (moveX != 0)
+		{
+			rb.velocity = new Vector2(moveX, rb.velocity.y);
+		}
+
+		// 스페이스 바나 터치로 점프
+		if (Input.GetKeyDown(KeyCode.Space) && jumpCount < maxJumps || Input.touchCount > 0 && jumpCount < maxJumps && !isJumping)
+		{
+			rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+			jumpCount++;
+			isJumping = true;
+		}
+	}
+
+	// 점프가 끝나면 isJumping을 false로 초기화
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.CompareTag("Ground"))
+		{
+			jumpCount = 0;
+			isJumping = false;
+		}
+	}
+}
